27224aa97029dcec819ffe0c76be441b440f8066
sikidal <edya.rosadi@yahoo.com>
2023-01-25 13:25:03 +0800

    menyusun ulang folder bagian

--- routes.php
+++ routes.php
@@ -9,19 +9,19 @@ if (isset($_GET['page'])) {
 switch ($page) {
     case "":
     case "dashboard":
-        include 'pages/dashboard.php';
+        include 'pages/bagian/dashboard.php';
         break;
     case "bagian":
-        include 'pages/bagian.php';
+        include 'pages/bagian/bagian.php';
         break;
     case "bagiantambah":
-        include 'pages/bagiantambah.php';
+        include 'pages/bagian/bagiantambah.php';
         break;
     case "bagianhapus":
-        include 'pages/bagianhapus.php';
+        include 'pages/bagian/bagianhapus.php';
         break;
     case "bagianubah":
-        include 'pages/bagianubah.php';
+        include 'pages/bagian/bagianubah.php';
         break;
     default:
         include 'pages/404.php';
